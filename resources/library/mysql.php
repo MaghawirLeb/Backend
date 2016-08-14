80a95e6480e821368ef1e55052be74e99e43f381
Michael Mouawad <michael_mouawad@aol.com>
2016-08-14 17:40:51 +0300

    mysqli: added insert and delete functions

--- resources/library/mysql.php
+++ resources/library/mysql.php
@@ -9,7 +9,7 @@ class MySql
     const MYSQL_PASS = "web";
     const MYSQL_DB = "demo";
 
-    public static function Select(string $query, $param_array = null, $mysqli = null) : array
+    public static function Select(string $query, $param_array = null, &$mysqli = null) : array
     {
         $rows = array();
         $close = false;
@@ -45,6 +45,69 @@ class MySql
 
             return $rows;
 
+        } finally {
+            if ($close) {
+                $mysqli->close();
+            }
+        }
+    }
+
+    public static function Insert(string $query, $param_array = null, &$mysqli = null) : int
+    {
+        $close = false;
+
+        if ($mysqli == null) {
+            $mysqli = self::OpenConnection();
+            $close = true;
+        }
+
+        try {
+            $stmt = $mysqli->prepare($query);
+            if (!$stmt) {
+                throw new Exception($mysqli->error);
+            }
+
+            if (!empty($param_array)) {
+                call_user_func_array(array($stmt, "bind_param"), $param_array);
+            }
+
+            if ($stmt->execute()) {
+                $response = $stmt->insert_id;
+                $stmt->close();
+            } else {
+                throw new Exception($stmt->error);
+            }
+
+            return $response;
+        } finally {
+            if ($close) {
+                $mysqli->close();
+            }
+        }
+    }
+
+    public static function Delete(string $table_name, int $id, &$mysqli = null)
+    {
+        $close = false;
+
+        if ($mysqli == null) {
+            $mysqli = self::OpenConnection();
+            $close = true;
+        }
+
+        try {
+            $result = $mysqli->query("SHOW KEYS FROM `$table_name` WHERE Key_name = 'PRIMARY'");
+            if ($result && $result->num_rows === 1) {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                $pk = $row["Column_name"];
+                $result->free();
+            }
+
+            if (isset($pk)) {
+                $mysqli->query("DELETE FROM `$table_name` WHERE `$pk`='$id'");
+            } else {
+                throw new Exception("Unable to find the primary key for the selected table");
+            }
         } finally {
             if ($close) {
                 $mysqli->close();
